72a1a78fe9104a50851375d3b475fb38a6cf9930
Nurbaki Vural <nurbakivural@gmail.com>
2023-05-16 03:28:03 -0700

    added auth

---
+++ src/components/Providers.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React, { ReactNode } from "react"
+import { Toaster } from "react-hot-toast"
+
+type Props = {
+  children: ReactNode
+}
+
+const Providers = ({ children }: Props) => {
+  return (
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      {children}
+    </>
+  )
+}
+
+export default Providers
